891f73343b7f43d306dba07096f8f9de858e9f2d
solidity <SolidityGo@outlook.com>
2022-10-08 11:18:36 +0800

    chores

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ const main = async () => {
 
 
   websocketProvider = new providers.WebSocketProvider(WEBSOCKET_URL);
-  const block = await websocketProvider.getBlock(21779350);
+  const block = await websocketProvider.getBlock(TARGET_HEIGHT);
   log(block);
 };
 
