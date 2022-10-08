5da5c5753427f280cda23ef18307065683cf4a12
solidity <SolidityGo@outlook.com>
2022-10-08 11:59:00 +0800

    chores

--- index.ts
+++ index.ts
@@ -14,14 +14,21 @@ const TOTAL_BLOCKS = 30 * 24 * 3600 / 3;  // block in 30 days
 
 const iFace = new utils.Interface([
   // "function exactInputSingle(tuple(address tokenIn, address tokenOut, uint25624 fee, address recipient, uint256 deadline, uint256 amountIn, uint256 amountOutMinimum, uint256160 sqrtPriceLimitX96) calldata) external payable returns (uint256 amountOut)",
-  "function removeLiquidityETH(address token, uint256 liquidity, uint256 amountTokenMin, uint256 amountETHMin, address to, uint256 deadline) external returns (uint256 amountToken, uint256 amountETH)",
+  // "function removeLiquidityETH(address token, uint256 liquidity, uint256 amountTokenMin, uint256 amountETHMin, address to, uint256 deadline) external returns (uint256 amountToken, uint256 amountETH)",
+
+  "function addLiquidity(address tokenA, address tokenB, uint256 amountADesired, uint256 amountBDesired, uint256 amountAMin, uint256 amountBMin, address to, uint256 deadline) external returns (uint256 amountA, uint256 amountB, uint256 liquidity)",
 ])
+
+const sigHash = iFace.getSighash("addLiquidity")
 
 const parseTx = async (tx: TransactionResponse) => {
   const data = tx.data
 
+  log('sigHash', sigHash)
+
   if (!data || data.length < 8) return
-  if (data.indexOf(iFace.getSighash("removeLiquidityETH")) < 0) return
+  if (data.indexOf(sigHash) < 0) return
+
 
   try {
     let parsedTx = iFace.parseTransaction(tx)
@@ -31,7 +38,6 @@ const parseTx = async (tx: TransactionResponse) => {
   } catch (e) {
     log('parse tx error', e)
   }
-
 
 }
 
@@ -57,9 +63,10 @@ const main = async () => {
 
     if (!block) continue
 
-    log('get block for ', currentHeight)
-
     const txs = block.transactions
+
+    log('get block for ', currentHeight, "txs", txs.length)
+
     checkTxs(txs).then()
   }
 };
