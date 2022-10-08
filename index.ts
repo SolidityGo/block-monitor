304e24cd09af576149d0db3cf3968541e93a139d
solidity <SolidityGo@outlook.com>
2022-10-08 10:56:25 +0800

    init

---
+++ index.ts
@@ -0,0 +1,19 @@
+import { ethers, utils, providers, BigNumber} from "ethers";
+const log = console.log;
+
+let websocketProvider;
+
+const WEBSOCKET_URL = 'ws://localhost:9046';
+
+const main = async () => {
+  websocketProvider = new providers.WebSocketProvider(WEBSOCKET_URL);
+  const block = await websocketProvider.getBlock(16737670);
+  log(block);
+};
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
